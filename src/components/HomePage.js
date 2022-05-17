8ee74ba57ee1d5a7fd7e50082d213555c9660d6a
unknown <ronaldwin79@hotmail.com>
2022-05-17 16:47:44 -0400

    add home and about components

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+    return (
+    <div className="jumbotron">
+        <h1>React Home Component</h1>
+        <p>React is a libarry created by facebook</p>
+        <a href="/about">About</a>
+    </div>
+    );
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/HomePage";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
